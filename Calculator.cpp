6b2c6d29bcc4fa90cd4ac36f3b96cffadb0b87d5
vrdalyan-star <vrdalyan4@gmail.com>
2026-06-07 15:31:15 +0700

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+string op;
+int a;
+int b;
+int c;
+cout<<"A Simple Calculator\n"<<endl;
+cin>>op;
+cin>>a;
+cin>>b;
+
+if (op == "+"){
+  c=a+b;
+   cout<<"Penjumlahan"<<endl;
+   cout<<a<<" + "<<b<<" = "<<c<<endl;
+ } 
+ 
+ else if (op == "-"){
+  c=a-b;
+   cout<<"Pengurangan"<<endl;
+   cout<<a<<" - "<<b<<" = "<<c<<endl;
+ } 
+ 
+ else if (op=="*"){
+  c=a*b;
+   cout<<"Perkalian"<<endl;
+   cout<<a<<" × "<<b<<" = "<<c<<endl;
+ } 
+ 
+ else if(op=="÷"){
+  c=a/b;
+   cout<<"Pembagian"<<endl;
+   cout<<a<<" ÷ "<<b<<" = "<<c<<endl;
+ } 
+ 
+ else if(op=="%"){
+  c=a%b;
+   cout<<"Sisa Bagi"<<endl;
+   cout<<a<<" % "<<b<<" = "<<c<<endl;
+ } else {
+ cout<<"Operator Tidak Dikenali"<<endl;
+ }
+
+return 0;
+}
